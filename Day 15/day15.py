2ab015ffc2a5178f6b1c990d27cd0f975c494de3
Marélette van Dalen <flakkenmarsh@gmail.com>
2022-12-09 13:25:44 +0100

    Complete day 15

--- Day 15/day15.py
+++ Day 15/day15.py
@@ -1,15 +1,15 @@
-file = open('input.csv', 'r')
-lines = file.readlines()
-lines = [line.strip('\n' ',') for line in lines]
+# file = open('15input.csv', 'r')
+# lines = file.readlines()
+lines = ["10,16,6,0,1,17"]  # [line.strip('\n' ',') for line in lines]
+# lines = ["0,3,6"]  # [line.strip('\n' ',') for line in lines]
 
 
-def setup():
+def part1():
     line = lines[0].split(",")
     line = [int(c) for c in line]
     turn = 1
     numbers = {}
     for i in line:
-        print(i, end=",")
         numbers[i] = []
         numbers[i].append(turn)
         turn += 1
@@ -17,21 +17,55 @@ def setup():
     spoken = 0
     while turn <= 2020:
         if numbers.__contains__(last) and len(numbers[last]) > 1:
-            spoken = turn - 1 - numbers[last][-1]
+            spoken = turn - 1 - numbers[last][-2]
+            if not numbers.__contains__(spoken):
+                numbers[spoken] = []
             numbers[spoken].append(turn)
         else:
             spoken = 0
             if not numbers.__contains__(spoken):
                 numbers[spoken] = []
             numbers[spoken].append(turn)
-            last = 0
         turn += 1
         line.append(spoken)
-        print(spoken, end=",")
-    print("")
-    print(line)
-    print(numbers)
+        last = spoken
+
+    print(line[-1])
+
+
+def add_to_dict(dict, key, val):
+    if not dict.__contains__(key):
+        dict[key] = []
+    dict[key].append(val)
+
+
+def puzzle(turns):
+    line = lines[0].split(",")
+    line = [int(c) for c in line]
+    turn = 1
+    numbers = {}
+    for i in line:
+        numbers[i] = []
+        numbers[i].append(turn)
+        turn += 1
+    last = line[-1]
+    spoken = 0
+    while turn <= turns:
+        if turn % 500000 == 0:
+            print(int(turn/500000), "/", int(turns/500000))
+        if numbers.__contains__(last) and len(numbers[last]) > 1:
+            spoken = turn - 1 - numbers[last][-2]
+        else:
+            spoken = 0
+
+        add_to_dict(numbers, spoken, turn)
+        turn += 1
+        line.append(spoken)
+        last = spoken
+
+    print(line[-1])
 
 
 if __name__ == "__main__":
-    setup()
+    # puzzle(2020)
+    puzzle(30000000)
